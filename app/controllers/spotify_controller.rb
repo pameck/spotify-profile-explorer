65bfdd93c9e6c015768769a85fc99ee944a1c038
Pam Rucinque <pamela.rucinque@gmail.com>
2016-08-17 16:29:31 +1000

    Authorizing Spotify

--- app/controllers/spotify_controller.rb
+++ app/controllers/spotify_controller.rb
@@ -1,7 +1,26 @@
+require 'cgi'
+require 'rest-client'
+
 class SpotifyController < ActionController::Base
   protect_from_forgery with: :exception
 
   def show
     render "index"
+  end
+
+  def authorize
+    go_to = CGI::escape('http://localhost:3000/spotify/validateauth')
+    @spotify_random = SecureRandom.hex
+
+    redirect_to("https://accounts.spotify.com/authorize/?client_id=#{ENV['SPOTIFY_CLIENT_ID']}&response_type=code&redirect_uri=#{go_to}&scope=user-read-private%20user-read-email&state=#{@spotify_random}",
+      {:cookies => {:auth_state => "#{@spotify_random}"}})
+  end
+
+  def authorize_finish
+    redirect_to "/spotify/dashboard"
+  end
+
+  def dashboard
+    render "dashboard"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/spotify', to: 'spotify#show'
+  get '/spotify/authorize', to: 'spotify#authorize'
+  get '/spotify/dashboard', to: 'spotify#dashboard'
+  get '/spotify/validateauth', to: 'spotify#authorize_finish'
+
 end
