b1ae803804b5eeb6624cb97b1eda3085205fe140
Pam Rucinque <pamela.rucinque@gmail.com>
2016-08-20 16:31:44 +1000

    Privacy matters

--- app/helpers/spotify.rb
+++ app/helpers/spotify.rb
@@ -2,15 +2,9 @@ module Spotify
 
   ALL_SCOPES = ['playlist-read-private',
     'playlist-read-collaborative',
-    'playlist-modify-public',
-    'playlist-modify-private',
-    'user-follow-modify',
     'user-follow-read',
     'user-library-read',
-    'user-library-modify',
     'user-read-private',
-    'user-read-birthdate',
-    'user-read-email',
     'user-top-read']
 
   ME_API_URL = 'https://api.spotify.com/v1/me'
