40f12d3836b878d7b3c4ca2d032d6904cafbcd46
Pam Rucinque <pamela.rucinque@gmail.com>
2016-08-22 13:55:25 +1000

    Removing email and active record support

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,16 @@
 require_relative 'boot'
 
-require 'rails/all'
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+# require "active_record/railtie"
+require "action_controller/railtie"
+# require "action_mailer/railtie"
+require "action_view/railtie"
+# require "action_cable/engine"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -13,3 +23,6 @@ module SpotifyProfileExplorer
     # -- all .rb files in that directory are automatically loaded.
   end
 end
+
+
+
